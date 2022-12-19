47956d5e35dcb109b1b12088ed1ad5028fce0117
Natalya <nataliettaa@mail.ru>
2022-12-19 13:54:49 +0500

    Solve Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(ReadLine());
+WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(ReadLine());
+if (b*b == a)
+{
+    WriteLine("-> ДА");
+}
+else
+{
+    WriteLine("-> НЕТ");
+}
