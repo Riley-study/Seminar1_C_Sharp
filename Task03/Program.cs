ead9414ab9c193c493658f7372637e8b53ac6133
Natalya <nataliettaa@mail.ru>
2022-12-19 14:26:47 +0500

    Solve task03

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+WriteLine("Введите число от 1 до 7: ");
+int a = Convert.ToInt32(ReadLine());
+if (a == 1) WriteLine("Понедельник");
+if (a == 2) WriteLine("Вторник");
+if (a == 3) WriteLine("Среда");
+if (a == 4) WriteLine("Четверг");
+if (a == 5) WriteLine("Пятница");
+if (a == 6) WriteLine("Суббота");
+if (a == 7) WriteLine("Воскресенье");
+else WriteLine("Введено неверное число");
