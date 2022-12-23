0384e34bbfbb82cff899543eeddb74a41b764afd
Natalya <nataliettaa@mail.ru>
2022-12-23 12:31:37 +0500

    solved task 11

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// **Задача 11:** Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+using static System.Console;
+Clear();
+int number = new Random().Next(100,1000);
+WriteLine(number);
+int a1 = number/100;
+int a2 = number/10;
+int a3 = number%10;
+WriteLine(a1*10+a3);
