c48a206e1dae0b979a4f346b1a9c6bb9cfe433fd
Natalya <nataliettaa@mail.ru>
2022-12-23 14:45:52 +0500

    Solved task 14

---
+++ Task14/Program.cs
@@ -0,0 +1,6 @@
+﻿// **Задача 14:** Напишите программу, которая принимает на вход число и проверяет, 
+//кратно ли оно одновременно 7 и 23.
+using static System.Console;
+int number = Convert.ToInt32(ReadLine());
+WriteLine(number%7==0? number%23==0? "Да":"Нет");
+
