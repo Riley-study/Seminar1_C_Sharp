239bcde935f4a00f12e13362c24546cf05a023da
Natalya <nataliettaa@mail.ru>
2022-12-23 12:23:11 +0500

    solved task 09

---
+++ Task09/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+using static System.Console;
+Clear();
+int number = new Random().Next(10,100);
+WriteLine(number);
+int a1=number/10;
+int a2=number%10;
+if (a1>a2)
+{
+    WriteLine(a1);
+}
+else
+{
+     WriteLine(a2);
+}
+// WriteLine(a1>a2?a1:a2); с использоанием тернарного оператора в одну строку!
