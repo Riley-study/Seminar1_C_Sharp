749a9312eae5a175246a5dbf86a78faff40df41f
Natalya <nataliettaa@mail.ru>
2022-12-27 14:46:55 +0500

    Solved task 19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние 
+// между ними в 2D пространстве.
+using static System.Console;
+Clear();
+
+WriteLine("Введите координату х точки А: ");
+int Ax = Convert.ToInt32(ReadLine());
+WriteLine("Введите координату y точки А: ");
+int Ay = Convert.ToInt32(ReadLine());
+WriteLine("Введите координату х точки B: ");
+int Bx = Convert.ToInt32(ReadLine());
+WriteLine("Введите координату y точки B: ");
+int By = Convert.ToInt32(ReadLine());
+double Length = Math.Sqrt(Math.Pow((Bx - Ax),2) + Math.Pow((By - Bx),2));
+WriteLine($"Длинна отрезка АВ : {Length}");
