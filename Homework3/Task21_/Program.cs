01c65aba6e85647b2bd7b472b8d8d6af268691dd
Natalya <nataliettaa@mail.ru>
2022-12-28 10:59:10 +0500

    Solved task 21

--- Homework3/Task21_/Program.cs
+++ Homework3/Task21_/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+using static System.Console;
+Clear();
+WriteLine("Введите координату х для точки А");
+int Ax = Convert.ToInt32(ReadLine());
+WriteLine("Введите координату y для точки А");
+int Ay = Convert.ToInt32(ReadLine());
+WriteLine("Введите координату z для точки А");
+int Az = Convert.ToInt32(ReadLine());
+WriteLine("Введите координату х для точки B");
+int Bx = Convert.ToInt32(ReadLine());
+WriteLine("Введите координату y для точки B");
+int By = Convert.ToInt32(ReadLine());
+WriteLine("Введите координату z для точки B");
+int Bz = Convert.ToInt32(ReadLine());
+double length = Math.Sqrt(Math.Pow((Ax-Bx),2)+Math.Pow((Ay-By),2)+Math.Pow((Az-Bz),2));
+WriteLine($"Расстояние между двумя точками: {length:f2}");
