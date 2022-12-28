e7fadc2c39d6823792ed0106f37cac581c3dce55
Natalya <nataliettaa@mail.ru>
2022-12-28 11:09:53 +0500

    Solved task 23

--- Homework3/Task23_/Program.cs
+++ Homework3/Task23_/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+using static System.Console;
+Clear();
+WriteLine("Введите число: ");
+int N = Convert.ToInt32(ReadLine());
+int cube = 1;
+while (cube<N) 
+{
+WriteLine($"Куб числа {cube} равен: {cube*cube*cube}");
+cube++;
+}
+
