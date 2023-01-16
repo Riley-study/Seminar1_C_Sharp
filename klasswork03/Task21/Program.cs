b24f5b7b881c541968491810810a394d7e88f286
Natalya <nataliettaa@mail.ru>
2023-01-16 09:11:28 +0500

    Starting solve task 21

---
+++ klasswork03/Task21/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+
+using static System.Console;
+Clear();
+WriteLine("Input number: ");
+int number = int.Parse(ReadLine()!);
+
+int sum = GetSumm(number);
+WriteLine($"Sum number 1...A = {sum}");
+
+int GetSumm(int A)
+{
+    int result = 0;
+    while (A>0)
+    {
+        result+=A;
+        A--;
+    }
+    return result;
+}
