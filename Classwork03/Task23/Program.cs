df3f30bb05cff889f602d01316f41a9f30f673b5
Natalya <nataliettaa@mail.ru>
2023-01-18 13:24:02 +0500

    solved task23

---
+++ Classwork03/Task23/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход число N и 
+// выдаёт произведение чисел от 1 до N.
+//4 -> 24
+//5 -> 120
+using static System.Console;
+Clear();
+
+WriteLine("Input number N: ");
+int number = Convert.ToInt32(ReadLine());
+
+//Обратимся или вызовем метод
+int FactorialResult = Factorial(number);
+WriteLine($"Factorial N (N!) = {FactorialResult}");
+
+// Напишем метод
+int Factorial(int A)
+{
+    int result = 1;
+    while (A>1)
+    {
+        result*=A;
+        A--;
+    }
+    return result;
+}
