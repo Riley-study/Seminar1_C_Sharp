f9abd0361e89ad2162f854a20e56f0f18a65976f
Natalya <nataliettaa@mail.ru>
2023-01-19 11:00:22 +0500

    Solve Task 38

--- Homework05/Task38/Program.cs
+++ Homework05/Task38/Program.cs
@@ -5,3 +5,64 @@
 
 using static System.Console;
 Clear();
+
+int[] InitialArray = GetArray(10, 10, 100);
+PrintArray(InitialArray);
+WriteLine();
+
+int Max = MaxValue(InitialArray);
+int Min = MinValue(InitialArray);
+int Diff =  Max - Min;
+WriteLine($"Максимальный элемент: {Max}");
+WriteLine($"Минимальный элемент: {Min}");
+WriteLine($"Разница между максимальным и минимальным элементами в массиве: {Diff}");
+
+
+
+
+// Метод, который ищет максимальное число в массиве
+int MaxValue(int[] inArray)
+{
+    int i = 0;
+    int MaxIndex = inArray[i];
+    for (i = 0; i < inArray.Length - 1; i++)
+    {
+        if (MaxIndex < inArray[i + 1]) MaxIndex = inArray[i + 1];
+    }
+    return MaxIndex;
+}
+
+// Метод, который ищет минимальное число в массиве
+int MinValue(int[] inArray)
+{
+    int i = 0;
+    int MinIndex = inArray[i];
+    for (i = 0; i < inArray.Length - 1; i++)
+    {
+        if (MinIndex > inArray[i + 1]) MinIndex = inArray[i + 1];
+    }
+    return MinIndex;
+}
+
+// метод, который формирует массив указанного диапозона
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] resultArray = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        resultArray[i] = rnd.Next(minValue, maxValue + 1);
+    }
+    return resultArray;
+}
+
+//Метод, который выводит на печать массив
+void PrintArray(int[] inArray)
+{
+    Write("[");
+    for (int i = 0; i < inArray.Length - 1; i++)
+    {
+        Write($"{inArray[i]}, ");
+    }
+    Write($"{inArray[inArray.Length - 1]}]");
+}
