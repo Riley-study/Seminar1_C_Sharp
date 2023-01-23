a0f49eea730fd864f95f74cc278d613946374872
Natalya <nataliettaa@mail.ru>
2023-01-23 09:12:00 +0500

    Solved task 40

--- Classwork06/Task40/Program.cs
+++ Classwork06/Task40/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, 
+// может ли существовать треугольник с сторонами такой длины.
+
+using static System.Console;
+Clear();
+
+// Треугольник со сторонами А В С, длинна сторон задается пользователем
+
+WriteLine("Введите длинну стороны А");
+int A = Convert.ToInt32(ReadLine());
+WriteLine("Введите длинну стороны B");
+int B = Convert.ToInt32(ReadLine());
+WriteLine("Введите длинну стороны C");
+int C = Convert.ToInt32(ReadLine());
+
+WriteLine(ItIsTriangle(A,B,C)? $"Треугольник со сторонами {A}, {B} и {C} существует":"Треугольник со сторонами {A}, {B} и {C} не существует" );
+
+// Метод, который проверят возможность существования треугольника с заданными сторонами
+bool ItIsTriangle(int a, int b, int c)
+{
+    if(a<(b+c) && b<(a+c) && c<(a+b)) return true;
+    return false;
+}
