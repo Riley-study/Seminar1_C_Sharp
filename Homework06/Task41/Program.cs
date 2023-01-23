e10376cea5986b8c76960b2f1960c8a846916922
Natalya <nataliettaa@mail.ru>
2023-01-23 14:29:49 +0500

    solved task 41

--- Homework06/Task41/Program.cs
+++ Homework06/Task41/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 
+// ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+using static System.Console;
+Clear();
+
+Write("Введите числа через пробел: ");
+string[] numbers = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+int[] ConvertArrayToInt = Array.ConvertAll(numbers, s => int.Parse(s));
+PrintArray(ConvertArrayToInt);
+WriteLine($" -> {CountNegative(ConvertArrayToInt)}");
+
+
+
+
+// Метод, который считает количество элементов меньше нуля в массиве
+int CountNegative(int[] inArray)
+{
+    int count=0;
+    for(int i=0; i<inArray.Length;i++)
+    {
+        if (inArray[i]<0) count++;
+    }
+    return count;
+} 
+
+//Метод, который выводит на печать массив
+void PrintArray(int[] inArray)
+{
+    Write("[");
+    for (int i=0; i< inArray.Length-1; i++)
+    {
+        Write($"{inArray[i]}, ");
+    }
+    Write($"{inArray[inArray.Length-1]}]");
+}
