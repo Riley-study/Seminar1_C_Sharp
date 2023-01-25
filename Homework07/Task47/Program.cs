a2b24f179034d3e113c65f9fb341c87212cd7c3f
Natalya <nataliettaa@mail.ru>
2023-01-25 14:59:49 +0500

    solve task 47

--- Homework07/Task47/Program.cs
+++ Homework07/Task47/Program.cs
@@ -10,21 +10,23 @@ Clear();
 
 WriteLine("Введите размер матрицы и диапозон значений через пробел");
 string[] parametrs = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
-float [,] InitArray = GetMatrixArrray(int.Parse(parametrs[0]), int.Parse(parametrs[1]),int.Parse(parametrs[2]),int.Parse(parametrs[3]));
+double [,] InitArray = GetMatrixArrray(int.Parse(parametrs[0]), int.Parse(parametrs[1]),int.Parse(parametrs[2]),int.Parse(parametrs[3]));
 WriteLine("Итоговый массив:");
 PrintMatrixArray(InitArray);
 
 // Метод,который формирует двумерный массив с указанным кол-ом строк и столбцов и 
 //заполняет его случайными значениями в указанном интервале
-float[,] GetMatrixArrray(int rows, int colomns, int minValue, int maxValue)
+double[,] GetMatrixArrray(int rows, int colomns, int minValue, int maxValue)
 {
     Random rnd = new Random();
-    float[,] resultArray = new float[rows, colomns];
+    double[,] resultArray = new double[rows, colomns];
     for (int i = 0; i < rows; i++)
     {
         for (int j = 0; j < colomns; j++)
         {
-            //resultArray[i, j] = rnd.NextDouble();
+            double number = rnd.NextDouble()*(maxValue-minValue)+minValue;
+            resultArray[i, j] = Math.Round(number,1);
+            //resultArray[i, j] = new Random().Next(minValue, maxValue+1);
         }
     }
     return resultArray;
@@ -32,7 +34,7 @@ float[,] GetMatrixArrray(int rows, int colomns, int minValue, int maxValue)
 }
 
 // Метод, выводящий массив на печать
-void PrintMatrixArray(float [,] inArray)
+void PrintMatrixArray(double [,] inArray)
 {
     for (int i = 0; i < inArray.GetLength(0); i++)
     {
