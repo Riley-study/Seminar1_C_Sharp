f3c180d176c4265acda9660144da3854664dd58d
Natalya <nataliettaa@mail.ru>
2023-01-25 13:44:43 +0500

    Librery example

---
+++ Libery/Task0/Program.cs
@@ -0,0 +1,10 @@
+﻿using static System.Console;
+// команды в консоль на память
+//dotnet new sln -o Tasks
+// dotnet new classlib
+// dotnet sln Tasks.sln add .\MyLib\MyLib.csproj
+// dotnet sln Tasks.sln add .\Task0\Task0.csproj
+// dotnet add .\Task0\Task0.csproj reference .\MyLib\MyLib.csproj
+
+using MyLib;
+MyMethod.GetLineArray;
