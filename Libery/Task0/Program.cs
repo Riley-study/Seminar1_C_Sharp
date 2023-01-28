6e7778e84c71a3ade36b53ff83c244d60cd98df5
Natalya <nataliettaa@mail.ru>
2023-01-28 13:15:43 +0500

    Solved task 55_0

--- Libery/Task0/Program.cs
+++ Libery/Task0/Program.cs
@@ -2,8 +2,8 @@
 // команды в консоль на память
 //dotnet new sln -o Tasks
 // dotnet new classlib
-// dotnet sln Tasks.sln add .\MyLib\MyLib.csproj
-// dotnet sln Tasks.sln add .\Task0\Task0.csproj
+// dotnet sln MyLib.sln add .\MyLib\MyLib.csproj
+// dotnet sln MyLib.sln add .\Task0\Task0.csproj
 // dotnet add .\Task0\Task0.csproj reference .\MyLib\MyLib.csproj
 
 using MyLib;
