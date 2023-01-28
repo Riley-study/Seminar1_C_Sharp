3b649ea41fc878e4ab911b100ae9116df48723f5
Natalya <nataliettaa@mail.ru>
2023-01-28 13:30:15 +0500

    Start solving task 54

---
+++ Homework08/Task54/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+
+
+
+
+//////////////////////////////////////////////// Methods ////////////////////////////////////////////////////
+
+//Метод, формирующий матрицу заданного размера в заданном диапозоне
+int[,] GetMatrixArray(int rows, int colomns, int minValue, int maxValue)
+{
+    int[,] resultArray = new int[rows,colomns];
+    Random rnd = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < colomns; j++)
+        {
+            resultArray[i,j] = rnd.Next(minValue, maxValue+1);
+        }
+    }
+    return resultArray;
+}
+
+
+// Метод, выводящий массив на печать
+void PrintMatrixArray(int [,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j],5}");
+        }
+        WriteLine();
+    }
+}
--- Libery/Task0/Program.cs
+++ Libery/Task0/Program.cs
@@ -2,8 +2,8 @@
 // команды в консоль на память
 //dotnet new sln -o Tasks
 // dotnet new classlib
-// dotnet sln MyLib.sln add .\MyLib\MyLib.csproj
-// dotnet sln MyLib.sln add .\Task0\Task0.csproj
+// dotnet sln Libery.sln add .\MyLib\MyLib.csproj
+// dotnet sln Libery.sln add .\Task0\Task0.csproj
 // dotnet add .\Task0\Task0.csproj reference .\MyLib\MyLib.csproj
 
 using MyLib;
