b367a7b59449372dd388e9b449e35e2fbf1045b9
Natalya <nataliettaa@mail.ru>
2023-01-31 12:42:20 +0500

    Solved task 67

---
+++ Classwork09/Task67/Program.cs
@@ -0,0 +1,24 @@
+﻿// **Задача 67:** Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+using static System.Console;
+Clear();
+
+Write($"{SumNumbers(5478)}");
+
+
+
+///////////////////////Methods//////////////////////
+
+int SumNumbers(int num)
+{
+    int sum = 0;
+    if (num == 0) return sum;
+    else
+    {
+sum = num%10 + SumNumbers(num/10);
+    }
+return sum;
+
+}
