0332e45884040e73a9de5da3806f55d11a854a75
Natalya <nataliettaa@mail.ru>
2023-02-01 09:07:05 +0500

    Solved task 66

--- Homework09/Task66/Program.cs
+++ Homework09/Task66/Program.cs
@@ -5,3 +5,33 @@
 
 using static System.Console;
 Clear();
+
+Write(PrintSumNumbers(1,15));
+
+
+
+
+////////////////////////////    Methods    /////////////////////////////////////
+// Метод, который находит сумму натуральных элементов в промежутке от M до N (рекурсивно)
+
+int PrintSumNumbers(int minValue, int maxValue)
+{
+    int sum = 0;
+    if (minValue > maxValue)
+    {
+        int temp = minValue;
+        minValue = maxValue;
+        maxValue = temp;
+    }
+    
+    if (minValue == maxValue)
+    {
+        sum+=sum + maxValue;
+    }
+    else
+    {
+        sum += minValue+PrintSumNumbers(minValue + 1, maxValue);
+        
+    }
+    return sum;
+}
